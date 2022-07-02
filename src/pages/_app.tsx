d05a04f7e8756f39906f431828548883f98ce46f
Prateek Soni <prateek_soni@outlook.com>
2022-07-02 17:24:50 +0530

    update env var
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,8 +46,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return {
